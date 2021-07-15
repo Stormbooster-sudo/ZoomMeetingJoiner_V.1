21915cc89a965cfb505cd599ea0e52b30429d442
Sahachat.H <77887797+Stormbooster-sudo@users.noreply.github.com>
2021-07-15 15:53:44 +0700

    Update rooms_csv.py

--- MeetingJoiner/rooms_csv.py
+++ MeetingJoiner/rooms_csv.py
@@ -2,10 +2,25 @@ import pandas as pd
 import os
 import sys
 
+#Direct to file more percisely.
 path = os.path.join(sys.path[0], "rooms.csv")
+
 def addData(newData):
+    df = pd.read_csv(path)
     data = pd.DataFrame(newData)
     data.to_csv(path, mode='a', encoding ='utf-8',index=False, header=False)
+    print(df)
+
+def deleteRow(i):
+    df = pd.read_csv(path)
+    df.drop(i,axis = 0, inplace=True)
+    df.to_csv(path,index = False)
+    print(df)
+
+def updateData(i,data):
+    df = pd.read_csv(path)
+    df.loc[i] = data
+    df.to_csv(path,encoding ='utf-8',index=False)
 
 def getLenght():
     df = pd.read_csv(path)
@@ -13,6 +28,6 @@ def getLenght():
     return lenght
 
 def getInfo():
-    df = pd.read_csv(path, converters={'passcode': lambda x: str(x)})
+    df = pd.read_csv(path,dtype=str)
     room_name, roomcode, passcode, username, time= df["room_name"],df['roomcode'],df['passcode'],df['username'],df['time']
     return room_name, roomcode, passcode, username, time
